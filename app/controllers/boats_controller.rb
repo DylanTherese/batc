503cd31fea6c9c8e928ef3f75b9ea67309b2a3bd
rico paum <ricsens@me.com>
2023-11-01 14:09:39 +0400

    implemented bookings controller logic and added to boats controller

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -24,6 +24,22 @@ class BoatsController < ApplicationController
     end
   end
 
+  def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+    @boat.update(task_params)
+    redirect_to boat_path(@boat)
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to boats_path, status: :see_other
+  end
+
   private
 
   def boat_params
