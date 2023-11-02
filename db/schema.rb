69ed6a77e7c11086efe4b1407abb6e364b194364
Ansaar Ramkoleea <ansaar.r.h@gmail.com>
2023-11-02 14:10:07 +0400

    pushing changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_064010) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_100157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,10 +54,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_064010) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.integer "day_price"
     t.string "cloudinary"
     t.string "cloudinary2"
     t.string "cloudinary3"
+    t.integer "day_price"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
