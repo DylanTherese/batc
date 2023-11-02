a2792da7389a2d6c87a2a259db329ac1bdd0b5b8
rico paum <ricsens@me.com>
2023-11-02 17:48:58 +0400

    user actions implemented

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,4 @@
 class Boat < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
   has_many :bookings
   belongs_to :user
   has_many_attached :photos
